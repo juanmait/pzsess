a22a6218a9d64bf0d14df6a04c2afc3dc57d0506
Juanma <juan.mc.dev@gmail.com>
2023-10-09 16:36:36 -0300

    document some fns, replace more unwraps by expects

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ const TEMP_SESS_BACKUP_FOLDER: &'static str = concat!(env!("HOME"), "/Zomboid/Sa
 /// The path to the folder from which pzload will look for previously saved sessions.
 const PZLOAD_SESSIONS_FOLDER: &'static str = concat!(env!("HOME"), "/Zomboid/BSaves");
 
+/// Build an [Iterator] over the directories (excluding files) inside the sessions folder.
 fn get_dirs_iter() -> impl Iterator<Item = std::fs::DirEntry> {
     std::fs::read_dir(PZLOAD_SESSIONS_FOLDER)
         .expect("Error trying to read_dir sessions folder.")
@@ -31,6 +32,9 @@ fn into_paths_iter(
     iter.map(|e| e.path())
 }
 
+/// Map an iterator over items of type [std::path::PathBuf] into an iterator of items of type [u128].
+/// Used to parse session folders (named with a timestamp in milliseconds since EPOCH) into the analog
+/// [u128] number so later we can easily sort the collection of sessions from oldest to newest.
 fn into_u128_timestamp(
     iter: impl Iterator<Item = std::path::PathBuf>,
 ) -> impl Iterator<Item = u128> {
@@ -44,10 +48,15 @@ fn into_u128_timestamp(
     })
 }
 
+/// Collect an iterator over [u128]s into a vector ([Vec<u128>]).
 fn iter_to_vec(iter: impl Iterator<Item = u128>) -> Vec<u128> {
     iter.collect()
 }
 
+/// Generate a sorted list of saved sessions from oldest to newest.
+/// At this point the returned _saved sessions_ is a sorted vector of [u128]s
+/// (timestamp in milliseconds since EPOCH in which the session was saved)
+/// ordered from _oldest_ to _newest_.
 fn get_sorted_list() -> Vec<u128> {
     let dir_iter = get_dirs_iter();
     let path_iter = into_paths_iter(dir_iter);
@@ -56,14 +65,18 @@ fn get_sorted_list() -> Vec<u128> {
     vec.sort();
     vec
 }
-/// Find the real index of a save that correspond to the given `n` number.
-/// This is mainly to support negative values for `n` so for example one
+
+/// Find the real index of a save that correspond to the given `n` number given as parameter.
+/// This is mainly to support negative values for the `n` parameter so for example one
 /// can obtain the last save using `n = -1` or the second to last save with
-/// `n = -2`.
-/// If n is a positive number this function will return the absolute value of
-/// it casted to `usize`.
+/// `n = -2` or the first one with `n = 0` etc.
+/// If `n` is a positive number this function will return the absolute value of
+/// it casted to a `usize`.
 fn get_n_timestamp_index(n: i32, list_len: usize) -> usize {
-    let n_absolute: usize = n.abs().try_into().unwrap();
+    let n_absolute: usize = n
+        .abs()
+        .try_into()
+        .expect("Error at trying to cast the given `i32` into `usize`.");
 
     if n.is_negative() {
         return list_len - n_absolute;
@@ -80,7 +93,12 @@ fn get_n_timestamp(n: i32) -> u128 {
     let list = get_sorted_list();
     let list_len = list.len();
     let index = get_n_timestamp_index(n, list_len);
-    let timestamp = list.get(index).unwrap();
+    let timestamp = list.get(index).expect(
+        format!(
+            "Index not found. No session was found in the given position {n} (computed index {index})"
+        )
+        .as_str()
+    );
     println!(
         "\nRecovering save with timestamp {timestamp} at position {index} of a total of {list_len} entries)\n\n",
     );
